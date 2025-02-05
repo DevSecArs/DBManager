038b261f76edea3abec698bf7f61eaf1007ce58a
PyArslan <agaev.arslan.com01@gmail.com>
2025-02-06 00:48:09 +0300

    Описаны функции:     - вывода приветсвия     - меню DataBaseManager     - вывод линии
    Реализовано меню DataBaseManager
        с обработкой запроса пользователяя

--- interface.py
+++ interface.py
@@ -1,4 +1,60 @@
 from api import CRUD, DataBaseManager
+        
+def menu():
+    print("""
+===========================================
+| Выберите действие:                      |
+|    1. Создать БД                        |
+|    2. Список БД                         |
+|    3. Удалить БД                        |
+|    4. Выбрать БД                        |
+|    5. Выйти                             |        
+===========================================
+    """)
+
+def greetings():
+    print("""
+===========================================
+| Добро пожаловать в менеджер баз данных! |""", end="")
+    menu()
+
+def line():
+    print("===========================================")
 
 if __name__ == "__main__":
-    pass
+    # Приветствие
+    greetings()
+
+    dbm = DataBaseManager()
+    while (True):
+        match (int(input("Ваш выбор: "))):
+            case 1:
+                # Создать БД
+                name = input("Введите название: ")
+                print(f"[{name}] успешно создана!") if (dbm.create(name)) else print(f"[{name}] уже существует!")
+            case 2:
+                # Вывести список БД
+                dbm.read()
+            case 3:
+                # Удалить БД
+                dbm.read()
+                name = input("Введите название: ")
+                if (input(f"Вы точно хотите удалить [{name}]? (Y/n): ").lower() == "y"):
+                    if (dbm.delete(name)): print(f"[{name}] успешно удалена.")
+                    else: print("Неверное имя базы данных!")
+            case 4:
+                # Выбрать БД
+                dbm.read()
+                name = input("Введите название: ")
+                if (dbm.exists(name)):
+                    cursor = CRUD("name")
+                    # match case CRUD
+                else:
+                    print("Неверное имя базы данных!")
+            
+            # Выход и обработка неверного ввода
+            case 5:
+                break
+            case _:
+                pass
+        line()
